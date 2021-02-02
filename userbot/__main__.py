7e3cf64705401e61dce4cdcf92373ac57c5641d3
FrosT2k5 <androygaming@gmail.com>
2021-02-02 14:02:42 +0530

    send message in botlog chat if alive

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,6 +6,8 @@
 """ Userbot start point """
 
 from importlib import import_module
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
 from sys import argv
 
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
@@ -26,12 +28,22 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running Black Heart")
+LOGS.info("You are running Project Fizilion")
 
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
-    "If you need assistance, head to https://t.me/BlackHeartUB")
+    "If you need assistance, head to https://t.me/ProjectFizilion")
 
+btlg = os.environ.get("BOTLOG") or "False"
+
+if btlg:
+    api = os.environ.get("API_KEY")
+    _hash = os.environ.get("API_HASH")
+    cid = os.environ.get("BOTLOG_CHATID")
+    with TelegramClient(StringSession(), api, _hash) as client:
+        msg = "Master, Fizilion is now alive, \nTest it by typing .alive in the chat"
+        client.send_message(cid, msg)
+    
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
