08d596f7f62a4018e0af09006d27a871f6cece8b
FrosT2k5 <androygaming@gmail.com>
2021-06-13 00:54:34 +0530

    fixup for sql 1.4+

--- userbot/modules/sql_helper/__init__.py
+++ userbot/modules/sql_helper/__init__.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from userbot import DB_URI
+import re
 
+if DB_URI.startswith("postgres://"):
+    uri = DB_URI.replace("postgres://", "postgresql://", 1)
+else:
+   uri = DB_URI
 BASE = declarative_base()
 
-
 def start() -> scoped_session:
-    engine = create_engine(DB_URI)
+    engine = create_engine(uri)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
